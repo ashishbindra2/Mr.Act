3029568ca6922285aaed053a283a59318f447664
Ashish bindra <ashishbindra2@gmail.com>
2020-05-28 22:15:49 +0530

    complete

---
+++ n/water_tank_booking.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Booking</title>
+    <link rel="stylesheet" href="./css/tank.css">
+</head>
+
+<body>
+    <section>
+        <h3>Residential Lead</h3>
+
+        <form action="" enctype="multipart/form-data" method="POST">
+            <div class="form-group">
+                <label for="">Customer Contact Number</label>
+                <input type="text" class="input-control" placeholder="Customer Contact Number">
+            </div>
+            <div class="form-group">
+
+                <label for="">Customer Name</label>
+                <input type="text" class="input-control" placeholder="Customer Name">
+            </div>
+            <div class="form-group">
+
+                <label for="">Location Address</label>
+                <input type="text" class="input-control" placeholder="House No . Stret No">
+            </div>
+            <div class="form-group">
+
+                <label for="">Landmark</label>
+                <input type="text" class="input-control" placeholder="Landmark">
+            </div>
+            <div class="form-group">
+
+                <label for="">City</label>
+                <input type="text" class="input-control" placeholder="City">
+            </div>
+            <div class="form-group">
+
+                <label for="">Number Of Tanks</label>
+                <input type="text" class="input-control" placeholder="Number Of Tanks">
+            </div>
+            <div class="form-group">
+
+                <label for="">Customer Email Id</label>
+                <input type="text" class="input-control" placeholder="Customer Email Id">
+            </div>
+        </form>
+        <button type="reset" class="re">Reset</button>
+        <button type="submit" class="sub">Submit</button>
+        <hr>
+    </section>
+</body>
+
+</html>
